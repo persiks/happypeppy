ecf328a8ed5f02464d5b2e95fd4d3dd241a09e08
Serhii <spersik@gmail.com>
2023-02-28 00:10:13 +0100

    start adptation for tablet

--- src/js/script.js
+++ src/js/script.js
@@ -126,7 +126,7 @@ $(document).ready(function(){
    
     $("a[href='#up']").click(function(){
         const _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        $("html, body").animate({scrollTop: $(_href).offset().top+50+"px"});
         return false;
     });
 
